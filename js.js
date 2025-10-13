d6038d62091f106de80d510bb67f88a9360db6f2
Maksym_Demydasiuk <maksdem1406@gmail.com>
2025-10-13 20:09:02 +0200

    .

---
+++ js.js
@@ -0,0 +1,9 @@
+$('.testimonials-box').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  arrows: true,
+  prevArrow: `<button type="button" class="slick-prev"> <img src="./img/Group 40.png"</button>`,
+  nextArrow: `<button type="button" class="slick-next"> <img src="./img/Group 39.png"</button>`
+});
